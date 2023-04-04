c8a69685402abdf4d7ea437b19302e51609ab3e5
juandacorrea112 <juandavidcorreaorozco74@gmail.com>
2023-04-04 14:23:18 -0500

    funcionalidad en proceso

--- js/presentaacion.js
+++ js/presentaacion.js
@@ -1,11 +1,15 @@
 $(function() {
+
+
+    
+
     $("#btnStart").click(function () {
        let lentDark = $("#darkWindow")
        lentDark.addClass('darkWindowClass')
 
        setTimeout(() => {
         window.location.assign('/pages/index.html')
-    }, 2500);
+    }, 3500);
     }) 
 })
 
